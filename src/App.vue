f4a4f4a8ad0bfff403b8f6a90f13c9c589130def
Sanjay <sanjayrachwani@yahoo.com>
2021-06-06 21:05:56 +0200

    Audiorecorder dependencies etc

--- src/App.vue
+++ src/App.vue
@@ -4,17 +4,36 @@
   <Button text="Home" />
   <p>add some space here between the buttons</p>
   <Button text="Start" button_color="green"></Button>
+  <audio-recorder 
+    upload-url = "./data"
+    :attempts="1"
+    :time="1"
+    :headers="headers"
+    :before-recording="callback"
+    :pause-recording="callback"
+    :after-recording="callback"
+    :select-record="callback"
+    :before-upload="callback"
+    :successful-upload="callback"
+    :failed-upload="callback"/>
 </template>
 
 <script>
 import Header from './components/Header.vue'
 import Button from './components/Button.vue'
+import AudioRecorder from 'vue-audio-recorder'
 
 export default {
   name: 'App',
   components: {
     Header,
     Button,
+    AudioRecorder,
+  },
+  methods: {
+    callback(data) {
+      console.debug(data)
+    }
   }
 }
 </script>
